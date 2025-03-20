1f8f6a02eca0df553766c536495edc659fcc5f5e
Leonardo Schunck Rainha <schunckrainhaleonardo@gmail.com>
2025-03-20 10:00:31 -0300

    feat: add DateTimePicker to CreateAppointmentScreen for scheduling appointments

--- src/screens/CreateAppointmentScreen.tsx
+++ src/screens/CreateAppointmentScreen.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components/native';
 import { Button } from 'react-native-elements';
 import { HeaderContainer, HeaderTitle } from '../components/Header';
 import theme from '../styles/theme';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { Text, View } from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import RNDateTimePicker from '@react-native-community/datetimepicker';
 
 type RootStackParamList = {
     Home: undefined;
@@ -16,6 +19,8 @@ type CreateAppointmentScreenProps = {
 };
 
 const CreateAppointmentScreen: React.FC<CreateAppointmentScreenProps> = ({ navigation }) => {
+    const [date, setDate] = useState(new Date())
+
     return (
         <Container>
             <HeaderContainer>
@@ -42,6 +47,18 @@ const CreateAppointmentScreen: React.FC<CreateAppointmentScreenProps> = ({ navig
 
                 {/* Aqui você pode adicionar os campos do formulário de agendamento */}
                 <FormText>Formulário de Agendamento</FormText>
+                <FormContent>
+                    <FormField>
+                        <FormLabelField>
+                            Selecione a data e o horário do seu agendamento
+                        </FormLabelField>
+                        <DateTimePicker
+                            mode='datetime'
+                            value={new Date()}
+                            onChange={(e) => setDate(new Date(e.nativeEvent.timestamp))}
+                        />
+                    </FormField>
+                </FormContent>
             </Content>
         </Container>
     );
@@ -62,5 +79,24 @@ const FormText = styled.Text`
   color: ${theme.colors.text};
   text-align: center;
 `;
+
+const FormContent = styled.View`
+    border: 1px solid ${theme.colors.primary};
+    border-radius: 20px;
+    padding: 20px;
+    margin-top: 20px;
+`
+
+const FormField = styled.View`
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: flex-end;
+    gap: 20px;
+`
+
+const FormLabelField = styled.Text`
+    color: #1818189c;
+`
 
 export default CreateAppointmentScreen;
